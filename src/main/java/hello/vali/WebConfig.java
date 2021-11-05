10f11def7580416abb6e96b319c0d2542bb789ad
taseon <ats3059@naver.com>
2021-11-05 19:57:56 +0900

    interceptor commit

---
+++ src/main/java/hello/vali/WebConfig.java
@@ -0,0 +1,16 @@
+package hello.vali;
+
+import hello.vali.interceptor.TestInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new TestInterceptor())
+                .order(1)
+                .addPathPatterns("/**");
+    }
+}
---
+++ src/main/java/hello/vali/interceptor/TestInterceptor.java
@@ -0,0 +1,19 @@
+package hello.vali.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class TestInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+        log.info("User Info : UUID =  [{}]  URI = [{}] " ,uuid,requestURI);
+        return true;
+    }
+}
